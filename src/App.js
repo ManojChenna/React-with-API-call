370a91086e9979a3a687b2bfe50007c5f48fb048
Manoj Chennareddy <52932901+ManojChenna@users.noreply.github.com>
2020-11-20 13:29:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default class App extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = {lat: null}
+}
+render() {
+  return <div>test</div>;
+}
+}
